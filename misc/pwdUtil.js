187af75dae12f0cfcd5f5c262e2f3021313a3e90
Hector Correa <hector@hectorcorrea.com>
2016-09-05 19:32:58 -0400

    Deleted obsolete Node.js files

--- misc/pwdUtil.js
+++
@@ -1,9 +0,0 @@
-// To calculate a salted password
-c = require('./models/cryptoUtil');
-console.log( c.createHash('pwd', 'salt')); 
-
-// To update the password in db
-// via the MongoDB shell
-mongo mongoUri -u mongoUser -p mongoPwd
-use dbname
-db.users.update({_id:ObjectId("user_id")}, {$set: {password : "salted_password"}})
