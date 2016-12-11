d99ca25bf338ca5d620a5a1a41bd8b0b2d6eb0b0
Hector Correa <hector@hectorcorrea.com>
2016-12-11 14:15:23 -0500

    Fixed bug in route order and removed dead code

--- web/blog.go
+++ web/blog.go
@@ -15,13 +15,14 @@ var blogRouter Router
 func blogPages(resp http.ResponseWriter, req *http.Request) {
 
 	// This should be initialized only once, not on every call.
+	// TODO: I also need a route with GET /blog/:title to handle legacy routes
 	blogRouter.Add("GET", "/blog/:title/:id", blogViewOne)
 	blogRouter.Add("GET", "/blog/", blogViewAll)
-	blogRouter.Add("POST", "/blog/new", blogNew)
 	blogRouter.Add("POST", "/blog/:title/:id/edit", blogEdit)
 	blogRouter.Add("POST", "/blog/:title/:id/save", blogSave)
 	blogRouter.Add("POST", "/blog/:title/:id/post", blogPost)
 	blogRouter.Add("POST", "/blog/:title/:id/draft", blogDraft)
+	blogRouter.Add("POST", "/blog/new", blogNew)
 
 	session := newSession(resp, req)
 	found, route := blogRouter.FindRoute(req.Method, req.URL.Path)
@@ -32,28 +33,6 @@ func blogPages(resp http.ResponseWriter, req *http.Request) {
 		renderNotFound(session)
 	}
 }
-
-// func blogPagesOld(resp http.ResponseWriter, req *http.Request) {
-//
-// 	session := newSession(resp, req)
-// 	if req.Method == "GET" {
-// 		blogView(session)
-// 	} else if req.Method == "POST" {
-// 		blogAction(session)
-// 	} else {
-// 		renderError(session, "Unknown HTTP Method", errors.New("HTTP method not supported"))
-// 	}
-// }
-
-// func blogView(s session) {
-// 	if id, err := parseBlogViewUrl(s.req.URL.Path); err != nil {
-// 		renderError(s, "Cannot parse Blog URL", err)
-// 	} else if id != 0 {
-// 		blogViewOne(s, id)
-// 	} else {
-// 		blogViewAll(s)
-// 	}
-// }
 
 func blogViewOne(s session, values map[string]string) {
 	id := idFromString(values["id"])
@@ -84,34 +63,15 @@ func blogViewAll(s session, values map[string]string) {
 	}
 }
 
-// func blogAction(s session) {
-// 	id, action, err := parseBlogEditUrl(s.req.URL.Path)
-// 	if err != nil {
-// 		renderError(s, "Cannot determine HTTP action", err)
-// 		return
-// 	}
-//
-// 	if !s.isAuth() {
-// 		renderNotAuthorized(s)
-// 		return
-// 	}
-//
-// 	if action == "new" {
-// 		blogNew(s)
-// 	} else if action == "edit" {
-// 		blogEdit(s, id)
-// 	} else if action == "save" {
-// 		blogSave(s, id)
-// 	} else if action == "post" {
-// 		blogPost(s, id)
-// 	} else if action == "draft" {
-// 		blogDraft(s, id)
-// 	} else {
-// 		renderError(s, "Unknown action", nil)
-// 	}
-// }
-
 func blogSave(s session, values map[string]string) {
+
+	// TODO: handle auth on all POST actions
+	// 	if !s.isAuth() {
+	// 		renderNotAuthorized(s)
+	// 		return
+	// 	}
+	//
+
 	id := idFromString(values["id"])
 	blog := blogFromForm(id, s)
 	if err := blog.Save(); err != nil {
@@ -192,48 +152,6 @@ func idFromString(str string) int64 {
 	id, _ := strconv.ParseInt(str, 10, 64)
 	return id
 }
-
-// func parseBlogViewUrl(url string) (id int64, err error) {
-// 	if url == "/blog/" {
-// 		return 0, nil
-// 	}
-// 	// url /blog/:title/:id
-// 	// parts[0] empty
-// 	// parts[1] blog
-// 	// parts[2] title
-// 	// parts[3] id
-// 	parts := strings.Split(url, "/")
-// 	if len(parts) == 4 && parts[0] == "" && parts[1] == "blog" {
-// 		return idFromString(parts[3])
-// 	}
-// 	return 0, errors.New("Could not parse (view) blog URL")
-// }
-//
-// func parseBlogEditUrl(url string) (id int64, action string, err error) {
-// 	if url == "/blog/new" {
-// 		return 0, "new", nil
-// 	}
-// 	// url /blog/:title/:id/:action
-// 	// parts[0] empty
-// 	// parts[1] blog
-// 	// parts[2] title
-// 	// parts[3] id
-// 	// parts[4] action (edit, post, draft)
-// 	parts := strings.Split(url, "/")
-// 	if len(parts) == 5 && parts[0] == "" && parts[1] == "blog" {
-// 		if id, err := idFromString(parts[3]); err != nil {
-// 			return 0, "", err
-// 		} else {
-// 			action := parts[4]
-// 			if action == "edit" || action == "save" ||
-// 				action == "post" || action == "draft" {
-// 				return id, action, nil
-// 			}
-// 			return 0, "", errors.New("Invalid action")
-// 		}
-// 	}
-// 	return 0, "", errors.New("Could not parse (edit) blog URL")
-// }
 
 func blogFromForm(id int64, s session) models.Blog {
 	var blog models.Blog
