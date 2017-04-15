f358e6f7520a8c37eb5e75df56705aa98afa5a18
Hector Correa <hector@hectorcorrea.com>
2017-04-15 13:13:40 -0400

    Adds task to re-save all blog posts

--- main.go
+++ main.go
@@ -11,9 +11,13 @@ import (
 func main() {
 	var address = flag.String("address", "localhost:9001", "Address where server will listen for connections")
 	var importer = flag.String("import", "", "Name of file to import legacy blog from")
+	var resave = flag.String("resave", "", "Resaves all blog posts to force recalculate of HTML content")
 	flag.Parse()
 	if *importer != "" {
 		importOne(*importer)
+		return
+	} else if *resave != "" {
+		resaveAll()
 		return
 	}
 	web.StartWebServer(*address)
@@ -25,4 +29,17 @@ func importOne(fileName string) {
 	}
 	log.Printf("Database: %s", models.DbConnStringSafe())
 	models.ImportOne(fileName)
+}
+
+func resaveAll() {
+	if err := models.InitDB(); err != nil {
+		log.Fatal("Failed to initialize database: ", err)
+	}
+	log.Printf("Database: %s", models.DbConnStringSafe())
+	blogs, _ := models.BlogGetAll(true)
+	for _, b := range blogs {
+		log.Printf("re-saving %d - %s", b.Id, b.Title)
+		blog, _ := models.BlogGetById(b.Id)
+		blog.Save()
+	}
 }
